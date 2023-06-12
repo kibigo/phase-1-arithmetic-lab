562656cfaca5ea7ddf996b2eca3e492b96fc0f1e
Kibigo <jacksonkibigo99@gmail.com>
2023-06-12 16:52:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1*num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 13;
+const num4 = 9;
+const mod = num3%num4;
+
+
+let max = Math.max(16,7,4,12,20);
