66d17cf0710f9ca58f3c62a9751549a031854d6c
Kibigo <jacksonkibigo99@gmail.com>
2023-07-03 22:38:50 +0300

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-let num1 = 31;
-let num2 = 2;
+const num1 = 31;
+const num2 = 2;
 
 let multiply = num1*num2;
-console.log(multiply);
+let random = Math.floor(Math.random()) +1;
 
-let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 13;
+let num4 = 9;
+let mod = num3%num4;
 
-const num3 = 13;
-const num4 = 9;
-const mod = num3%num4;
-
-
-let max = Math.max(16,7,4,12,20);
+let max = Math.max (16,9,20,4,7);
